b43a1d41be22dec968dad7f8d87e73b06c1477c2
teddiechan <96613621+teddiechan@users.noreply.github.com>
2021-12-24 15:19:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "merry chít mớt",
+    introTitle: 'Hi chào bạn nka',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnIntro: ':3',
+    title: 'Bạn cũm thích mình đúng ko :>',
+    desc: 'Phải thích chứ uwu ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích mình :0',
+    btnReply: 'Gửi',
+    reply: 'Tại bạn vừa đẹp trai vừa hiền',
+    mess: 'Mình biết mà 😏',
+    messDesc: 'Làm ngiu mình nka :>',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/teddie.anh' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
